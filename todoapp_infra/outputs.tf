8041f54795a3b2ab0a7ea82528c7f32617319481
DevOps Insiders <info@devopsinsiders.com>
2024-02-11 02:27:18 +0530

    updated code

--- todoapp_infra/outputs.tf
+++ todoapp_infra/outputs.tf
@@ -6,16 +6,25 @@ output "vnet_subnet_ids" {
   value = module.networking.vnet_subnet_ids
 }
 
-# output "vm_private_ips" {
-#   value = module.vms.vm_private_ips
-# }
+output "vm_private_ips" {
+  value = module.vms.vm_private_ips
+}
 
-# output "vm_public_ips" {
-#   value = module.vms.vm_public_ips
-# }
+output "vm_public_ips" {
+  value = module.vms.vm_public_ips
+}
 
-# output "vm_nic_ids" {
-#   value = module.vms.vm_nic_ids
-# }
+output "vm_nic_ids" {
+  value = module.vms.vm_nic_ids
+}
+
+output "vm_ids" {
+  value = module.vms.vm_ids
+}
+
+output "db_connection_strings" {
+  value = module.database.db_connection_strings
+}
+
 
 
